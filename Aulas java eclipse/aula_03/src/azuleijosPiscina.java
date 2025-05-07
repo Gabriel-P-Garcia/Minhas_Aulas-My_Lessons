2c985f35be03a7cf2bbf61f86f888d1d4cfe589a
Gabriel Garcia <gpgarcia14@hotmail.com>
2025-05-07 13:29:05 -0300

    Add files via upload

---
+++ Aulas java eclipse/aula_03/src/azuleijosPiscina.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class azuleijosPiscina {
+ public static void main(String[] args) {
+	Scanner leitor = new Scanner(System.in);
+
+//Declaração de variáveis
+	double comprimento = 0;
+	double largura = 0;
+	double profundidade = 0;
+	double tamanhoazuleijo = 0;
+	double azuleijonecessarios = 0;
+	
+//Entrada de dados
+	System.out.print("Comprimento da piscina: ");
+		comprimento = leitor.nextDouble();
+		
+	System.out.print("Largura da piscina: ");
+		largura = leitor.nextDouble();
+		
+	System.out.print("Profundidade da piscina: ");
+		profundidade = leitor.nextDouble();
+		
+	System.out.print("Tamanho do azuleijo: ");
+		tamanhoazuleijo = leitor.nextDouble();
+	
+	leitor.close();
+//Processamento
+	azuleijonecessarios = ((comprimento * largura) + (profundidade * largura * 2) + (comprimento * profundidade * 2) / Math.pow(tamanhoazuleijo, 2));
+	
+//Saida de dados
+	System.out.print("Vão ser necessários " + azuleijonecessarios + " azuleijos");
+ }
+ 
+}
